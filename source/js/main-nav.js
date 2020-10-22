3ec5a5e7b6357a088b09bf10f601bf71943010d6
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-10-22 17:36:07 +0300

    index

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,6 +2,8 @@ const mainNav = document.querySelector('.main-navigation');
 const btnOpen = document.querySelector('.main-navigation__button--open')
 const btnClose = document.querySelector('.main-navigation__button--close')
 
+mainNav.classList.add('main-navigation--closed');
+
 btnClose.addEventListener('click', () => {
     mainNav.classList.add('main-navigation--closed');
 })
